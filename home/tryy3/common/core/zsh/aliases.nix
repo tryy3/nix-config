37f108ff8ef289790c63e713ee1778eb3cb59dd0
tryy3 <admin@tryy3.dev>
2026-04-03 22:50:45 +0200

    small fixes

--- home/tryy3/common/core/zsh/aliases.nix
+++ home/tryy3/common/core/zsh/aliases.nix
@@ -1,4 +1,4 @@
-{ osConfig, ... }:
+{ ... }:
 let
   devDirectory = "$HOME/src";
   devNix = "${devDirectory}/nix";
@@ -83,8 +83,8 @@ in
 
   #-------------SSH---------------
   ssh = "TERM=xterm ssh";
-  pinghosts = "nmap -sP ${osConfig.hostSpec.networking.subnets.grove.cidr}";
-  scanhostson10022 = "sudo nmap -sS ${osConfig.hostSpec.networking.subnets.grove.cidr} -p ${toString osConfig.hostSpec.networking.ports.tcp.ssh}";
+  # pinghosts = "nmap -sP ${osConfig.hostSpec.networking.subnets.grove.cidr}";
+  # scanhostson10022 = "sudo nmap -sS ${osConfig.hostSpec.networking.subnets.grove.cidr} -p ${toString osConfig.hostSpec.networking.ports.tcp.ssh}";
 
   #-------------rmtrash---------------
   # Path to real rm and rmdir in coreutils. This is so we can not use rmtrash for big files
